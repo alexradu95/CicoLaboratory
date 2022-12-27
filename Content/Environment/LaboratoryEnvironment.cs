97cf2bc2a0170b29798d47f0d218bc9869124499
Alex Radu <radualexandrucosmin@gmail.com>
2022-12-27 14:40:41 +0200

    Added new demos

---
+++ Content/Environment/LaboratoryEnvironment.cs
@@ -0,0 +1,58 @@
+﻿using StereoKit;
+using StereoKit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicoLaboratory.Content.Environment
+{
+    // Laboratory environment from JumbliVR repository
+    internal class LaboratoryEnvironment : IStepper
+    {
+        public bool Enabled => throw new NotImplementedException();
+
+        Random r = new Random();
+        Dictionary<Vec4, Color> env = new Dictionary<Vec4, Color>();
+
+        public bool Initialize()
+        {
+            BuildEnvironment();
+            return true;
+        }
+
+        public void Shutdown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Step()
+        {
+            foreach (KeyValuePair<Vec4, Color> kvp in env)
+                Mesh.Sphere.Draw(Material.Default, Matrix.TS(kvp.Key.XYZ, SKMath.Lerp(.1f, .5f, kvp.Key.w)), kvp.Value);
+
+            Text.Add("North", Matrix.TRS(Vec3.Forward * 5f, Quat.FromAngles(0, 180, 0), 3), Color.Black);
+            Text.Add("South", Matrix.TRS(Vec3.Forward * -5f, Quat.FromAngles(0, 0, 0), 3), Color.Black);
+            Text.Add("East", Matrix.TRS(Vec3.Right * 5f, Quat.FromAngles(0, 90, 0), 3), Color.Black);
+            Text.Add("West", Matrix.TRS(Vec3.Right * -5f, Quat.FromAngles(0, 270, 0), 3), Color.Black);
+            Text.Add("Home", Matrix.TRS(Vec3.Up * -1.6f, Quat.FromAngles(90, 180, 0), 3), Color.Black);
+        }
+
+        void BuildEnvironment()
+        {
+
+            int count = 100;
+            int envRange = 5;
+            while (count > 0)
+            {
+                Vec4 v = new Vec4(r.NextInt64(envRange * -1, envRange), r.NextInt64(envRange * -1, envRange), r.NextInt64(envRange * -1, envRange), r.NextSingle());
+                if (env.ContainsKey(v) == false)
+                {
+                    env.Add(v, Color.HSV(r.NextSingle(), .5f, .5f));
+                    count--;
+                }
+            }
+        }
+    }
+}
