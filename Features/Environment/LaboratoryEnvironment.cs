0f305d7e3f79fca9400ad4808d1d36345a9c7284
Alex Radu <radualexandrucosmin@gmail.com>
2023-01-04 18:48:13 +0200

    Added floor generation

--- Features/Environment/LaboratoryEnvironment.cs
+++ Features/Environment/LaboratoryEnvironment.cs
@@ -6,54 +6,54 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace CicoLaboratory.Content.Environment
 {
     // Laboratory environment from JumbliVR repository
     internal class LaboratoryEnvironment : IStepper
     {
+        Model model;
+        const int cacheCount = 1000;
+        List<Pose> poseCache = new List<Pose>(cacheCount);
         public bool Enabled => throw new NotImplementedException();
-        Model apartmentModel;
-        Random r = new Random();
-        Dictionary<Vec4, Color> env = new Dictionary<Vec4, Color>();
-
         public bool Initialize()
         {
-            BuildEnvironment();
+            for (int i = 0; i < cacheCount; i++)
+            {
+                poseCache.Add(Pose.Identity);
+            }
             return true;
         }
 
-        public void Shutdown()
-        {
-            throw new NotImplementedException();
-        }
+        public void Shutdown() { }
 
         public void Step()
         {
-            var apartmentTransform = Matrix.S(new Vec3(0.01f, 0.01f, 0.01f));
-            apartmentModel.Draw(apartmentTransform);
-            foreach (KeyValuePair<Vec4, Color> kvp in env)
-                Mesh.Sphere.Draw(Material.Default, Matrix.TS(kvp.Key.XYZ, SKMath.Lerp(.1f, .5f, kvp.Key.w)), kvp.Value);
+            Pose curr = poseCache[0];
 
+            DrawFloor();
             Text.Add("North", Matrix.TRS(Vec3.Forward * 5f, Quat.FromAngles(0, 180, 0), 3), Color.Black);
             Text.Add("South", Matrix.TRS(Vec3.Forward * -5f, Quat.FromAngles(0, 0, 0), 3), Color.Black);
             Text.Add("East", Matrix.TRS(Vec3.Right * 5f, Quat.FromAngles(0, 90, 0), 3), Color.Black);
             Text.Add("West", Matrix.TRS(Vec3.Right * -5f, Quat.FromAngles(0, 270, 0), 3), Color.Black);
             Text.Add("Home", Matrix.TRS(Vec3.Up * -1.6f, Quat.FromAngles(90, 180, 0), 3), Color.Black);
+
+
+            poseCache.RemoveAt(poseCache.Count - 1);
+            poseCache.Insert(0, curr);
         }
 
-        void BuildEnvironment()
+        private void DrawFloor()
         {
-            apartmentModel = Model.FromFile("Apartment/apartment.obj");
-            int count = 100;
-            int envRange = 5;
-            while (count > 0)
+            for (int x = 0; x < 100; x++)
             {
-                Vec4 v = new Vec4(r.NextInt64(envRange * -1, envRange), r.NextInt64(envRange * -1, envRange), r.NextInt64(envRange * -1, envRange), r.NextSingle());
-                if (env.ContainsKey(v) == false)
+                for (int z = 0; z < 100; z++)
                 {
-                    env.Add(v, Color.HSV(r.NextSingle(), .5f, .5f));
-                    count--;
+                    Vec3 grid = new Vec3(x, 0, z);
+                    Pose pose = poseCache[(int)((cacheCount - 1))];
+                    pose.position += grid;
+                    Mesh.Cube.Draw(Material.Default, pose.ToMatrix(0.9f));
                 }
             }
         }
