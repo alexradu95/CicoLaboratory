ea5868d830f3ff9634f3b548b58ce578b98f5ba3
Alex Radu <radualexandrucosmin@gmail.com>
2023-01-05 21:23:52 +0200

    Managed to extract each gif frame

--- Features/ShowImage.cs
+++ Features/ShowImage.cs
@@ -20,13 +20,13 @@ namespace CicoLaboratory.Features
     {
         public bool Enabled => throw new NotImplementedException();
         Pose windowPose = new Pose(-.4f, 0, 0, Quat.LookDir(1, 0, 1));
-        ImageFrameCollection framesCollection;
+        List<Byte[]> gifFramesInByteArray;
         Sprite logoSprite;
         int currentFrameNumber = 0;
 
         public bool Initialize() {
 
-            framesCollection = GetGifFramesAsync("https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif").Result;
+            gifFramesInByteArray = GetGifFramesAsync("https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif").Result;
 
             return true;
         }
@@ -39,48 +39,37 @@ namespace CicoLaboratory.Features
         public void Step()
         {
             UI.WindowBegin("Window", ref windowPose, new Vec2(20, 0) * U.cm, UIWin.Body);
-            AnimateGif();
             UI.WindowEnd();
         }
 
-        private void AnimateGif()
-        {
-            using (MemoryStream ms = new MemoryStream())
-            {
-                if(logoSprite == null) {
-                    logoSprite = Sprite.FromTex(Tex.FromMemory(ms.GetBuffer()));
-                }
 
-                Image currentFrame = framesCollection.CloneFrame(currentFrameNumber);
-                currentFrame.Save(ms, new GifEncoder());
-                logoSprite.Draw(Matrix.Identity, Color32.BlackTransparent);
-                
-            }
-
-            if (currentFrameNumber == framesCollection.Count - 1)
-            {
-                currentFrameNumber = 0;
-            } else
-            {
-                currentFrameNumber++;
-            }
-        }
-
-        public async Task<ImageFrameCollection> GetGifFramesAsync(string imageUrl)
+        public async Task<List<byte[]>> GetGifFramesAsync(string imageUrl)
         {
             using var httpClient = new HttpClient();
             using var response = await httpClient.GetAsync(imageUrl);
 
+            var gifFramesArray = new List<Byte[]>();
+
             if (response.IsSuccessStatusCode)
             {
                 using var stream = await response.Content.ReadAsStreamAsync();
-                return Image.Load(stream).Frames;
+                var framesCollection = Image.Load(stream).Frames;
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    for (int i = 0; i < framesCollection.Count; i++)
+                    {
+                        Image currentFrame = framesCollection.CloneFrame(currentFrameNumber);
+                        currentFrame.Save(ms, new GifEncoder());
+                        gifFramesArray.Add(ms.ToArray());
+                    }
+                }
+
+                return gifFramesArray;
+
             }
-            else
-            {
-                // Handle unsuccessful response
-                return null;
-            }
+
+            return null;
         }
 
     }
