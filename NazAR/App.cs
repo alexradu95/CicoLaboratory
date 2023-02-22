e3f8aa9e0c11505a370edbf92cb9e2ef99218fa0
Radu Alexandru <alexradu95@proton.me>
2023-02-22 08:34:10 -0800

    Working version

---
+++ NazAR/App.cs
@@ -0,0 +1,36 @@
+﻿using Nazar.Core;
+using StereoKit;
+using StereoKit.Framework;
+
+namespace NazAR
+{
+    public class App : IStepper
+    {
+        public App()
+        {
+            SK.AddStepper<CoreFeatures>();
+        }
+
+        public SKSettings Settings => new()
+        {
+            appName = "nazar.OS",
+            assetsFolder = "Assets",
+            displayPreference = DisplayMode.MixedReality
+        };
+
+        public bool Enabled => true;
+
+        public bool Initialize()
+        {
+            return true;
+        }
+
+        public void Step()
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+    }
+}
