e3acbb6077a1cdab4e1daeb31166aacf59a8f523
Radu Alexandru <alexradu95@proton.me>
2023-03-31 08:36:16 -0700

    Made infrastructure

--- Nazar/Launcher.cs
+++ Nazar/Launcher.cs
@@ -6,31 +6,18 @@ namespace NazAR
     public class Launcher : Node
     {
 
-        private Mesh _mesh;
-        private Material _material;
-
-        public Launcher()
-        {
-
-
-        }
+        private readonly HierarchyExample hierarchyExample = new HierarchyExample();
 
         public override bool Initialize()
         {
-
-            _mesh = Mesh.GenerateCube(Vec3.One * 1f);
-            _material = Material.Default;
+            AddChild(hierarchyExample);
 
             return base.Initialize();
-
-
         }
 
         public override void Step()
         {
             base.Step();
-
-            _mesh.Draw(_material, base.Pose.ToMatrix());
         }
 
         public SKSettings Settings => new()
